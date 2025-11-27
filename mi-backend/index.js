fc1529894e0f893851103ecc53afbbca4211fe5b
AdixonJ <julcaadixon25@gmail.com>
2025-11-27 11:23:55 -0500

    prueva

--- mi-backend/index.js
+++ mi-backend/index.js
@@ -12,3 +12,14 @@ app.use("/api/productos", createProductosRouter(pool));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`corriendo en el puerto${PORT}`));
+
+app.get("/test-db", async (req, res) => {
+    try {
+      const [rows] = await pool.query("SELECT 1 + 1 AS solution");
+      res.json(rows);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Error en conexión a BD" });
+    }
+  });
+  
